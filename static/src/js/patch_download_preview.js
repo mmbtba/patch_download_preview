347d4ae1ebdc65042dcbcbae577c727ecf70ae66
mmbtba <mmbtba@qq.com>
2014-10-28 06:29:21 +0800

    available for odoo v8
    Signed-off-by: mmbtba <mmbtba@qq.com>

--- static/src/js/patch_download_preview.js
+++ static/src/js/patch_download_preview.js
@@ -18,67 +18,51 @@
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################*/
-openerp.patch_download_preview = function(instance){
-  var _t = instance.web._t,
-  _lt = instance.web._lt;
-  var QWeb = instance.web.qweb;
-  
-  instance.web.Session.include({
-    get_file: function (options){
-      return this.open_preview(options);
-      var token = new Date().getTime();
-      var params = _.extend({}, options.data || {}, {token: token});
-      var url = this.url(options.url, params);
-      /*$('<a href="'+url+'" target="_blank"></a>')[0].click();*/
-      window.open(url);
-      if (options.success) { options.success(); }
-      if (options.complete) { options.complete(); }
-      return;
-    },
-    open_preview:function (options){
-      var token = new Date().getTime();
-      var id = _.uniqueId('get_file_frame');
-      
-      var params = _.extend({}, options.data || {}, {token: token});
-      var url = this.url(options.url, params);
-      // iOS devices doesn't allow iframe use the way we do it,
-      // opening a new window seems the best way to workaround
-      if (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {          
-          instance.web.unblockUI();
-          return window.open(url);
-      }
-      $preview = $(QWeb.render('PreviewWidget', {
-        title:_lt("Preview"), 
-        url:url, 
-        id:id, 
-        name:id
-        })).appendTo(document.body);
-      $preview.find("iframe").load(function () {
-          $preview.find(".preview_header").show();
-          if (options.success) { options.success(); }
-          if (options.complete) { options.complete(); }
-        });
-      $preview.on('hidden.bs.modal', this, function() {
-          $preview.remove();
-        });
-      $preview.modal({
-          'backdrop': false,
-        });
-      $preview.modal('show');
-      return;
-      var $target = $('<iframe frameborder="0" style="width:100%; height:100%">')
-        .attr({id: id, name: id})
-        .load(function () {          
-          if (options.success) { options.success(); }
-          if (options.complete) { options.complete(); }
-        });
-      var dialog = new instance.web.Dialog(this, {
-        title: _lt("preview"),
-      }, $target).open();
-      dialog.on('closing', this, function (e){
-        $target.remove();
-      });
-      $('<a href="'+url+'" target="'+id+'"></a>')[0].click();
-    }
-  });
+openerp.patch_download_preview = function(instance) {
+	var _t = instance.web._t, _lt = instance.web._lt;
+	var QWeb = instance.web.qweb;
+
+	instance.web.Session.include({
+		get_file : function(options) {
+			return this.open_preview(options);
+		},
+		open_preview : function(options) {
+			var token = new Date().getTime();
+			var id = _.uniqueId('get_file_frame');
+
+			var params = _.extend({}, options.data || {}, {
+				token : token
+			});
+			var url = this.url(options.url, params);
+			// iOS devices doesn't allow iframe use the way we do it,
+			// opening a new window seems the best way to workaround
+			if (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
+				instance.web.unblockUI();
+				return window.open(url);
+			}
+			$preview = $(QWeb.render('PreviewWidget', {
+				title : _lt("Preview"),
+				url : url,
+				id : id,
+				name : id
+			})).appendTo(document.body);
+			$preview.find("iframe").load(function() {
+				$preview.find(".preview_header").show();
+				if (options.success) {
+					options.success();
+				}
+				if (options.complete) {
+					options.complete();
+				}
+			});
+			$preview.on('hidden.bs.modal', this, function() {
+				$preview.remove();
+			});
+			$preview.modal({
+				'backdrop' : false,
+			});
+			$preview.modal('show');
+			return;
+		}
+	});
 };
